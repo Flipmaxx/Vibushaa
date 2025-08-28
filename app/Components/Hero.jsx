12eb98b7df04366d440a6c760f0e3c75c00bc4c9
Flipmaxx <flipmaxxglobal@gmail.com>
2025-08-28 17:04:49 +0530

    asd

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="w-full h-screen relative">
       {/* Desktop / Tablet Image */}
-    <img src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg" alt="" />
+    <img src="/Videos/M.jpg" alt="" />
 
    
       <Image
